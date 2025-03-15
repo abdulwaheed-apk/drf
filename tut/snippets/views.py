26f644bc35a50f637b292b989533add043f3ad91
Abdul Waheed <ab.waheed1722@gmail.com>
2025-03-15 22:44:53 +0300

    Hyperlinking our API

--- tut/snippets/views.py
+++ tut/snippets/views.py
@@ -9,7 +9,11 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
 from snippets.permissions import isOwnerOrReadOnly
+from rest_framework.reverse import reverse
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -31,4 +35,19 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadOnly]
-    
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
